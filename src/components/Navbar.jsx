15729cd65bc040fda0510b8d147bb16e2e7bc19c
shahzamaahmad <dev.shahzama@gmail.com>
2023-01-17 02:57:38 +0530

    firts commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import nftc from './images/nftcc.png';
+import aazadi from './images/aazadi.png'
+import swatch from './images/swatch.png'
+import Navigation from './Navigation';
+const Navbar = () => {
+  return (
+    <>
+      <div className='flex flex-row justify-between border-2 mt-5 place-content-center items-center'>
+        <div className='flex flex-row'>
+          <h3 className=' ml-14 mr-4 '>भारत सरकार</h3>
+          <h3 className=''>GOVERMENT OF INDIA</h3>
+        </div>
+        <div className='flex flex-row justify-between items-center'>
+          <h3><a href="#">SKIP TO MAIN CONTENT</a></h3>
+          <img src="https://dummyimage.com/40x40" alt="" />
+          <img src="https://dummyimage.com/40x40" alt="" />
+          <img src="https://dummyimage.com/40x40" alt="" />
+          <img src="https://dummyimage.com/40x40" alt="" />
+          <h3 className='mr-14'> <a href="#">हिन्दी</a> </h3>
+        </div>
+
+      </div>
+
+      <div className='flex flex-row justify-between mt-1 mx-14 '>
+        <div className='flex felx-row items-center place-content-center'>
+          <div className=''><img width={200} height={200} src={nftc} alt="image not found " /></div>
+          <div className='font-sans'>
+            <h1>National Federation of Tourism</h1>
+            <h1>and Transport Cooperative of India Ltd.</h1></div>
+        </div>
+        <div className='flex felx-row'>
+          <div><img width={200} height={200} src={aazadi} alt="" /></div>
+          <div className='mt-9'><img width={200} height={200} src={swatch} alt="" /></div>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default Navbar
+// border - solid
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <ul className='flex flex-row justify-around bg-blue-500 text-white items-center font-bold '>
+          <li className='bg-red-600'><img src="https://dummyimage.com/40x40" alt="image" srcset="" /></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Tourism Project</a></li>
+          <li><a href="#">Transport Project</a></li>
+          <li><a href="#">Other Project</a></li>
+          <li><a href="#">Products</a></li>
+          <li><a href="#">Media</a></li>
+          <li><a href="#">RTI</a></li>
+          <li><a href="#">Tenders</a></li>
+          <li><a href="#">Public Grivancas</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navigation

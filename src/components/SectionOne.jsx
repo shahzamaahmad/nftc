5aa21a1566e5c36598ae528239c120252158af94
PRIYANSHI <psrivastava@theallsafe.com>
2023-02-01 15:52:45 +0530

    coomittt

--- src/components/SectionOne.jsx
+++ src/components/SectionOne.jsx
@@ -3,23 +3,30 @@ import React from 'react'
 const SectionOne = () => {
   return (
     <>
-      <div className='w-full flex flex-row justify-between'>
+      <div className=''>
         <div className=' w-2/4 '>
 
           <div className='flex flex-row justify-between mb-10 mt-10'>
-            <div><img src="https://dummyimage.com/300x200" alt="" srcset="" /></div>
+            <div><img src="https://dummyimage.com/250x200" alt="" srcset="" /></div>
             <div><img src="https://dummyimage.com/300x200" alt="" srcset="" /></div>
             <div><img src="https://dummyimage.com/300x200" alt="" srcset="" /></div>
           </div>
 
           <div className='flex flex-row justify-between ' >
-            <div><img src="https://dummyimage.com/300x200" alt="" srcset="" /></div>
-            <div><img src="https://dummyimage.com/300x200" alt="" srcset="" /></div>
-            <div><img src="https://dummyimage.com/300x200" alt="" srcset="" /></div>
+            <div><img src="https://dummyimage.com/250x250" alt="" srcset="" /></div>
+            <div><img src="https://dummyimage.com/250x250" alt="" srcset="" /></div>
+            <div><img src="https://dummyimage.com/250x250" alt="" srcset="" /></div>
           </div>
         </div>
         <div className='w-2/4'>
-          <div></div>
+          <div>
+          <div className='flex flex-row justify-between mb-10 mt-10'>
+            <div><img src="https://dummyimage.com/300x350" alt="" srcset="" /></div>
+            <div><img src="https://dummyimage.com/300x350" alt="" srcset="" /></div>
+            <div><img src="https://dummyimage.com/300x350" alt="" srcset="" /></div>
+
+            </div>
+          </div>
         </div>
       </div>
 
